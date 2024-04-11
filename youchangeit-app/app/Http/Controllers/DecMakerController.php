ecd8d1547ba9db8b0364a5f96f5c58d9e04e63ef
andre-bos <mailbox@andreaboschetto.it>
2024-04-11 20:03:36 +0200

    migrazioni database corrette

---
+++ youchangeit-app/app/Http/Controllers/DecMakerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDecMakerRequest;
+use App\Http\Requests\UpdateDecMakerRequest;
+use App\Models\DecMaker;
+
+class DecMakerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDecMakerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DecMaker $decMaker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DecMaker $decMaker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDecMakerRequest $request, DecMaker $decMaker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DecMaker $decMaker)
+    {
+        //
+    }
+}
